1afb37cca4c1921d1d3663293a900ce0820e1d79
Ravi <ravipanchalvishwakarma@gmail.com>
2022-03-21 19:28:22 +0530

    1st version -item list and search working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { monsters: [], searchTerm: '' }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => ({ monsters: users })))
+  }
+
+  onSerchTermChange = (e) => { this.setState({ searchTerm: e.target.value.toLocaleLowerCase() }) }
+
+  render() {
+
+    const { monsters, searchTerm } = this.state;
+
+    const filteredMoster = monsters.filter(monster => monster.name.toLocaleLowerCase().includes(searchTerm))
+
+    return (
+      <div >
+        <input
+          placeholder='Search here' value={searchTerm}
+          onChange={this.onSerchTermChange}
+        />
+
+        <br />
+
+        {filteredMoster.map((item, index) => (
+          <div key={index}>
+            <h1>{item.name}</h1>
+          </div>
+        ))}
+
+      </div>
+    );
+  }
 }
 
 export default App;
+
